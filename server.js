ae2402d4e4819230160f473426da5aca65aac61b
Clayton Goff <cltngoff@gmail.com>
2021-06-06 16:17:54 -0700

    user routes written and express added

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+const express = require('express');
 const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
     useFindAndModify: false,
@@ -7,3 +17,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network'
 });
 
 mongoose.set('debug', true);
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
