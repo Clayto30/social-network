da3cad75c6b7816b31396ca8c405eeaa819db34c
Clayton Goff <cltngoff@gmail.com>
2021-06-06 20:06:13 -0700

    email validation

--- models/User.js
+++ models/User.js
@@ -8,7 +8,15 @@ const UserSchema = new Schema({
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: function validateEmail(email) {
+            const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(email).toLowerCase());
+        },
+        message: props => 'Please enter your email.'
+            
+    }
         // must match a valid email address (look into Mongoose's matching validation)
     },
     thoughts: [
