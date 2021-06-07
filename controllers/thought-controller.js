4b5576e23e980821bca2284f2bb4f3a6033b7c7a
Clayton Goff <cltngoff@gmail.com>
2021-06-06 17:55:36 -0700

    thoughts added to User model, and reactions schema

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,63 +1,87 @@
-const { Thought } = require('../models');
+const { Thought, User } = require('../models');
 
 const thoughtController = {
     // get all thoughts
     getAllThought(req, res) {
         Thought.find({})
-        .then(dbThoughtData => res.json(dbThoughtData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
     },
 
     // get one thought by id 
     getThoughtById({ params }, res) {
         Thought.findOne({ _id: params.id })
-        .then(dbThoughtData => {
-            if (!dbThoughtData) {
-                res.status(404).json({ message: 'No thought found with this id!' });
-                return;
-            }
-            res.json(dbThoughtData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
     },
 
     // create a new thought
-    createThought({ body }, res) {
-        THought.create(body)
-        .then(dbTHoughtData => res.json(dbTHoughtData))
-        .catch(err => res.status(400).json(err));
+    createThought({ params, body }, res) {
+        console.log(body);
+        Thought.create(body)
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $push: { thoughts: _id } },
+                    {new: true } 
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
     },
 
     // update any thought by id
     updateThought({ params, body }, res) {
         Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
-        .then(dbThoughtData => {
-            if (!dbThoughtData) {
-                res.status(404).json({ message: 'No thought found with this id!' });
-                return;
-            }
-            res.json(dbThoughtData);
-        })
-        .catch(err => res.status(400).json(err));
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(400).json(err));
     },
 
     // delete a thought
     deleteThought({ params }, res) {
-        Thought.findOneAndDelete({ _id: params.id })
-        .then(dbThoughtData => {
-            if (!dbThoughtData) {
-                res.status(404).json({ message: 'No thought found with this id!' });
-                return;
-            }
-            res.json(dbThoughtData);
-        })
-        .catch(err => res.status(400).json(err));
+        Thought.findOneAndDelete({ _id: params.thoughtId })
+            .then(deletedThought => {
+                if (!deletedThought) {
+                    return res.status(404).json({ message: 'No thought with this id!' });
+                }
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $pull: { thoughts: params.thoughtId } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
     }
 };
 
--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 const ThoughtSchema = new Schema({
     thoughtText: {
@@ -6,6 +6,7 @@ const ThoughtSchema = new Schema({
         // String, required, must be between 1 and 280 characters
     },
     createdAt: {
+        // module 18.2.7 has information on date formatting
         type: Date,
         default: Date.now
         // date, set default value to the current timestamp, use a getter method to format the timestamp on query
@@ -15,9 +16,33 @@ const ThoughtSchema = new Schema({
         // the user that created this thought, string, required
     },
     reactions: []
-        // these are like replies, array of nested documents created with the reactionSchema
+    // these are like replies, array of nested documents created with the reactionSchema
 });
 
+const ReactionSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment _id
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            // required
+            // 280 character maximum
+        },
+        username: {
+            type: String
+            // required
+        },
+        createdAt: {
+            // module 18.2.7 has information on date formatting
+            type: Date,
+            default: Date.now
+            // date, set default value to the current timestamp, use a getter method to format the timestamp on query
+        }
+    }
+)
 const Thought = model('Thought', ThoughtSchema);
 
 module.exports = Thought;
